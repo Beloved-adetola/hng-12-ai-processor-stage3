cdc17b243e92ac37726b78b5abcab9d33ae0efde
Adetola Beloved <belovedadetola@gmail.com>
2025-02-21 00:02:51 +0100

    2nd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <header className="">
       <nav className="">
-        <h2 className="">Ai Text Processor</h2>
+        <h2 className="">AI Text Processor</h2>
       </nav>
     </header>
   );
