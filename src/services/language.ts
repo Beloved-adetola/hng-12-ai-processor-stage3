3e39a01fdd002a4d449905c71bf3f1ff3db63d48
Adetola Beloved <belovedadetola@gmail.com>
2025-02-21 19:54:42 +0100

    last commit

--- src/services/language.ts
+++ src/services/language.ts
@@ -5,4 +5,9 @@ export const Languages = [
   { key: "ru", name: "Russian" },
   { key: "tr", name: "Turkish" },
   { key: "fr", name: "French" },
+  { key: "de", name: "German" },
+  { key: "it", name: "Italian" },
+  { key: "ja", name: "Japanese" },
+  { key: "ko", name: "Korean" },
+  { key: "zh", name: "Chinese" },
 ];
